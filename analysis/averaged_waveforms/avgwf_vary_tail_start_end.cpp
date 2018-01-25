5983004e2398413810bd3e4d9e8c8427b02626c7
namcneal <namcneal@umich.edu>
2018-01-25 12:18:51 -0500

    Added more code to analyze the qvalues of the averaged waveforms. Removed the separate .cpp/.h files to hold the qvalue calculation function because it caused more trouble than it was worth

--- analysis/averaged_waveforms/avgwf_vary_tail_start_end.cpp
+++ analysis/averaged_waveforms/avgwf_vary_tail_start_end.cpp
@@ -32,9 +32,8 @@
 # include "TPaveText.h"
 # include "TColor.h"
 
-#include "qval_calculation_functions.h"
-
 using namespace std;
+
 const double PERTDC=2.;
 const int N_width=15;
 const double photon_low=-20.;
@@ -44,13 +43,17 @@ const double neutron_high=150.;
 const double fit_left=-20.;
 const double fit_right_long=200.;
 const double fit_right=75.;
+
+void calculateQVal(double tailStart, double tailEnd, 
+				   TProfile* avgNeutron, TProfile* avgPhoton,
+				   double &neutronQVal, double &photonQVal);
 					  
 int main(int argc, char **argv){
 	cout << endl << endl << "Loading average waveform data" << endl;
 	string dataFileName = argv[2],
 		   dataFolderPath = argv[1];
 	
-	TFile* waveformFile=new TFile((dataFolderPath + dataFileName + "_average_waveforms.root").c_str(), "READ");
+	TFile* waveformFile=new TFile((dataFolderPath + '/' + dataFileName + "_average_waveforms.root").c_str(), "READ");
 	TProfile *avgNeutronWaveform,
 			 *avgPhotonWaveform;
 	
@@ -66,26 +69,71 @@ int main(int argc, char **argv){
 	
 	cout << "Setting analysis parameters" << endl;
 	const Int_t numSteps = 10;
-	
-				
+		
 	const double minTailStart = 10,
 				 maxTailStart = 80,
 				 minTailEnd = 90,
 				 maxTailEnd = 200,
 				 startStepSize = abs(maxTailStart - minTailStart) / numSteps,
 				 endStepSize   = abs(maxTailEnd   - minTailEnd)   / numSteps;
-		   
+				 
+	TGraph2D *neutronQVals = new TGraph2D(numSteps),
+			 *photonQVals  = new TGraph2D(numSteps);
+	neutronQVals->SetName("Neutron Q-Values");
+	photonQVals->SetName("Photon Q-Values");
+	
+	int i = 0;
 	cout << "Starting Q value calculations" << endl << "\tStep...";
 	for(tailStart = minTailStart; tailStart < maxTailStart; tailStart += startStepSize){
 		for(tailEnd = minTailEnd; tailEnd < maxTailEnd; tailEnd += endStepSize){
 
-			calculateQVals(tailStart, tailEnd, 
-						   avgNeutronWaveform, avgPhotonWaveform,
-						   neutronQVal, photonQVal);	
-						   
-			}
+			calculateQVal(tailStart, tailEnd, 
+						  avgNeutronWaveform, avgPhotonWaveform,
+						  neutronQVal, photonQVal);
+
+			neutronQVals->SetPoint(i, tailStart, tailEnd, neutronQVal);
+			 photonQVals->SetPoint(i, tailStart, tailEnd, photonQVal );
+									
 		}
+		
+		i++;
+
 	}
 	cout << endl << "Q-value calculations completed." << endl << endl;
+//	
+//	TCanvas *diffCanvas = new TCanvas("qValDiff", "Q-value Difference", 700, 600);
+//	qValDiffPlot->Draw("COLZ");
+//	
+//
+//	diffCanvas->SaveAs((dataFolderPath + dataFileName + "_varied_start_tail_difference.png").c_str(), "png");
+	
 	return 0;
+}
+
+void calculateQVal(double tailStart, double tailEnd, 
+				   TProfile* avgNeutron, TProfile* avgPhoton,
+				   double &neutronQVal, double &photonQVal){
+	
+	int tailStartBin = avgNeutron->FindBin(tailStart),
+		tailEndBin   = avgNeutron->FindBin(tailEnd);
+		
+	double a_main_n=0.,
+		   a_tail_n=0.,
+		   a_main_p=0.,
+		   a_tail_p=0.;
+	
+	for (int i=1; i<tailStartBin; i++){
+		a_main_n+=avgNeutron->GetBinContent(i);
+		a_main_p+=avgPhoton->GetBinContent(i);
+	}
+	for (int i = tailEndBin; i<=avgNeutron->FindBin(199.75); i++){
+		a_tail_n+=avgNeutron->GetBinContent(i);
+		a_tail_p+=avgPhoton->GetBinContent(i);
+	}
+	
+	double neutronCurrentQVal = a_tail_n / (a_tail_n+a_main_n),
+		   photonCurrentQVal  = a_tail_p / (a_tail_p+a_main_p);
+		   
+	neutronQVal = neutronCurrentQVal;
+	photonQVal  = photonCurrentQVal;
 }
--- analysis/qval_calculation_functions.h
+++
@@ -1,6 +0,0 @@
-#include "TProfile.h"
-
-void calculateQVal(double tailStart, double tailEnd, 
-				   TProfile* neutronWaveform, TProfile* photonWaveform,
-				   double &neutronQVal, double &photonQVal);
-				   

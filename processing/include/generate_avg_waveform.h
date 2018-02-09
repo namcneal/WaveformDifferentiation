6e5b0a4663953fea7ccdd2dabf6630d59aa86b20
namcneal <namcneal@umich.edu>
2018-02-08 23:35:37 -0500

    Started redoing the files for Q-value calculations

--- processing/include/generate_avg_waveform.h
+++ processing/include/generate_avg_waveform.h
@@ -16,8 +16,5 @@ const double photon_low=-20.;
 const double neutron_low=30.;
 const double photon_high=20.;
 const double neutron_high=150.;
-const double fit_left=-20.;
-const double fit_right_long=200.;
-const double fit_right=75.;
 
 void generateAverageWaveforms(std::string dataFolderPath, std::string dataFileName, int numBins);
